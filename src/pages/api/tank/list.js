4a5c635c5380dc62946f675dd6bcbbedfd19c9a2
ajish <azisgodgod@gmail.com>
2026-04-29 10:57:28 +0700

    penambahan fitur

--- src/pages/api/tank/list.js
+++ src/pages/api/tank/list.js
@@ -1,4 +1,4 @@
-import dbConnect from '../../../lib/dbConnect';
+import dbConnect from '../../../lib/mongodb';
 import Tank from '../../../models/Tank';
 
 export default async function handler(req, res) {
